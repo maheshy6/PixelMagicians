28d209b0c09876128ea80bc44ed2e7094534dbe8
maheshy6 <maheshy675@gmail.com>
2025-02-09 22:22:04 +0530

    d3

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDB =async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_CLOUD_URL);
         console.log("Connected to DB");
     } catch (error) {
         console.error("Connection failed", error.message);
--- server/src/models/maintenance.model.js
+++ server/src/models/maintenance.model.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const MaintenanceSchema = new mongoose.Schema({
-    vehicle: { type: mongoose.Schema.Types.ObjectId, ref: "Vehicle", required: true },
+    vehicle: { type: String, required: true },
     maintenanceType: { type: String, required: true },
-    scheduledDate: { type: Date, required: true },
+    scheduledDate: { type: String, required: true },
     status: { type: String, enum: ["Pending", "Completed"], default: "Pending" },
-    cost: { type: Number },
+    cost: { type: String },
     notes: { type: String },
     createdAt: { type: Date, default: Date.now },
     email: { type: String, required:true }
